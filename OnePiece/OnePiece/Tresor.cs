a573ef0de0dec4d5c4dfdc4d9979d3aebd0f5575
Jogigo <a2006266@institutmontilivi.cat>
2022-01-05 15:02:11 +0100

    Versio amb classes

---
+++ OnePiece/OnePiece/Tresor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnePiece { 
+    public class Tresor : Posicio
+    {
+        public Tresor(int fila, int columna) : base(fila, columna) { }
+        public override bool EsRobot { get => false; }
+        public override bool EsTresor { get => true; }
+
+    }
+}
