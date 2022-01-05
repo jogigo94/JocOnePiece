9e656ed3ece82d5fab0afe845b9787245b1ecf34
Jogigo <a2006266@institutmontilivi.cat>
2022-01-05 15:08:50 +0100

    Versio amb imatges

--- OnePiece/OnePiece/MainWindow.xaml.cs
+++ OnePiece/OnePiece/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace OnePiece
 {
@@ -20,9 +21,24 @@ namespace OnePiece
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int NFILES = 3, NCOLUMNES = 3;
+        Tauler joc;
+
+        DispatcherTimer timer = new DispatcherTimer();
         public MainWindow()
         {
+            joc = new Tauler(NFILES, NCOLUMNES);
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
             InitializeComponent();
+            grdTaulell.Children.Add(joc);
+            grdTaulell.Background = Brushes.CadetBlue;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            joc.posicioRobot();
         }
     }
 }
